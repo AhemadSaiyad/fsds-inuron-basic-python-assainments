a9e8639a3c0e0b8f338a13c14812c6637b67d41c
AhemadSaiyad <109778589+AhemadSaiyad@users.noreply.github.com>
2022-07-22 11:22:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+1.  In the below elements which of them are values or an expression? eg:- values can be
+
+integer or string and expressions will be mathematical operators.
+
+*        ................expression
+
+'hello'  ................value
+
+-87.8    ................value
+
+-        ................expression
+
+/        .................expression
+
++        .................expression
+
+6        ..................value
+
+2. What is the difference between string and variable?
+
+String: String is just text ,it is type of informetion, it can be written in quotes such: "ahmead", 'ahemad'
+
+Variable: Variable is a store of information
+
+3. Describe three different data types.
+
+List: sing of list is [] , it is used for storing multiple data items in a single variable.
+
+Integer: integer can store only numerical values.
+
+Float: float can store only floating values such as 1.2,1.3,1.4 etc.
+
+4. What is an expression made up of? What do all expressions do?
+
+Expression is made up of values,mathematical operators, containers,  e.g
+
+     a = 2 + 3
+
+expressions are used to evaluate the values or represent the result on the screen.
+
+5. This assignment statements, like spam = 10. What is the difference between an expression and a statement?
+
+expression is a combination of values and functions that are combined and interpreted by the compiler
+to create a new value,
+Expression is made up of values, containers, and mathematical operators
+and
+the statement is just like a command that a python interpreter executes like print.it doesn't return anything
+
+6. After running the following code, what does the variable bacon contain?
+
+bacon = 22
+
+bacon + 1
+
+Sol: print (bacon + 1)
+it gives 23
+
+
+7. What should the values of the following two terms be?
+
+7.1 'spam' + 'spamspam'
+sol : spamspamspam
+
+'spam' * 3
+sol : spamspamspam
+sol: it will be the same
+
+
+8. Why is eggs a valid variable name while 100 is invalid?
+
+Ans: we can not start giving variable an integer name.
+we should begin with, a string-like alphabet name then integer.
+e.g.  a100 is valid
+
+9. What three functions can be used to get the integer, floating-point number, or string version of a value?
+
+ans: int()
+     float(),
+     str()
+
+10. Why does this expression cause an error? How can you fix it?
+
+'I have eaten' + 99 + 'burritos'
+
+sol: 99 is an integer it cannot be concatenated with strings
+     if we have to concatenate it we need to convert 99 to string like "99"
+     print('I have eaten' + "99" + 'burritos')
+
+
