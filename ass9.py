2d220cfa844f51a85e0ed84f8d84a748863e94f4
AhemadSaiyad <109778589+AhemadSaiyad@users.noreply.github.com>
2022-09-17 19:54:23 -0700

    fsds python basic ass9

---
+++ ass9.py
@@ -0,0 +1,35 @@
+'''
+1.To what does a relative path refer?
+Ans.Relative path refers to  location that is relative to  current directory.
+
+2.What does an absolute path start with your operating system?
+Ans.Absolute path names always begin with the (/) slash.
+
+3.What do the functions os.getcwd() and os.chdir() do?
+Ans.os.getcwd() shows you current working directroy.
+os.chdir() use for change the current working directory to specified path.
+
+4.What are the . and .. folders?
+Ans.The . folder is the current folder,
+ and .. is the parent folder.
+
+5.In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+Ans. dir name : C:\bacon\eggs
+     base name :  spam.txt
+
+6.What are the three “mode” arguments that can be passed to the open() function?
+Ans. 'r' for read mode,
+     'w' for write mode,
+     'a' for append mode
+
+7.What happens if an existing file is opened in write mode?
+Ans.we can erased and completely overwritten.
+
+8.How do you tell the difference between read() and readlines()?
+Ans.The read() method give us entire contents of files as a single string value.
+ where The readlines() method returns a list of strings.
+
+9.What data structure does a shelf value resemble?
+Ans.that work similarly to the dictionary methods.
+
+'''
