a272de621cf808b911833edc14da58c52b0f726e
AhemadSaiyad <109778589+AhemadSaiyad@users.noreply.github.com>
2022-09-21 23:32:09 +0530

    fsds python basic ass13

---
+++ ass13.py
@@ -0,0 +1,24 @@
+'''
+1.What advantages do Excel spreadsheets have over CSV spreadsheets?
+Ans.In a xlsx you can save the formulas, graphs, pivots, etc in the file.
+ A CSV is just a flat text file that uses a delimiter to separated the fields,
+ Excel is more superior and better for storing more complex data,
+
+2.What do you pass to csv.reader() and csv.writer() to create reader and writer objects?
+Ans.DictWriter. The csv module also provides us the DictReader and DictWriter classes,
+ which allow us to read and write to files using dictionary objects.
+
+3.What modes do File objects for reader and writer objects need to be opened in?
+Ans. "w+"
+
+4.What method takes a list argument and writes it to a CSV file?
+Ans.writerows()
+
+5.What do the keyword arguments delimiter and line terminator do?
+Ans.The delimiter is the character that appears between cells on a row.in csv files "," is the delimiter
+and the line terminator is a newline.
+
+6.What function takes a string of JSON data and returns a Python data structure?
+Ans.loads()
+
+'''
